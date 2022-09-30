f60d2ad2ee23093208cf2376f8d355053419d200
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-10-01 00:20:19 +0800

    setup custom fonts

--- App.js
+++ App.js
@@ -7,6 +7,11 @@ import HomeScreen from "./screens/HomeScreen";
 import SettingsScreen from "./screens/SettingsScreen";
 import SavedScreen from "./screens/SavedScreen";
 import { Entypo, Ionicons } from "@expo/vector-icons";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useLayoutEffect, useState } from "react";
+import * as Font from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 
 const Tab = createBottomTabNavigator();
 
@@ -50,9 +55,46 @@ const TabNavigator = () => {
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [appIsLoaded, setAppIsLoaded] = useState(false);
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await Font.loadAsync({
+          black: require("./assets/fonts//Roboto-Black.ttf"),
+          blackItalic: require("./assets/fonts/Roboto-BlackItalic.ttf"),
+          bold: require("./assets/fonts/Roboto-Bold.ttf"),
+          boldItalic: require("./assets/fonts/Roboto-BoldItalic.ttf"),
+          italic: require("./assets/fonts/Roboto-Italic.ttf"),
+          light: require("./assets/fonts/Roboto-Light.ttf"),
+          lightItalic: require("./assets/fonts/Roboto-LightItalic.ttf"),
+          medium: require("./assets/fonts/Roboto-Medium.ttf"),
+          mediumItalic: require("./assets/fonts/Roboto-MediumItalic.ttf"),
+          regular: require("./assets/fonts/Roboto-Regular.ttf"),
+          thin: require("./assets/fonts/Roboto-Thin.ttf"),
+          thinItalic: require("./assets/fonts/Roboto-ThinItalic.ttf"),
+        });
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setAppIsLoaded(true);
+      }
+    };
+    prepare();
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (appIsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsLoaded]);
+
+  if (!appIsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
-      <View style={{ flex: 1 }}>
+      <View onLayout={onLayout} style={{ flex: 1 }}>
         <Stack.Navigator screenOptions={{ headerTitleAlign: "center" }}>
           <Stack.Group>
             <Stack.Screen
--- assets/fonts/SettingsScreen.js
+++ assets/fonts/SettingsScreen.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function SettingsScreen() {
   return (
     <View style={styles.container}>
-      <Text>Saved Screen</Text>
+      <Text>Settings Screen</Text>
     </View>
   );
 }
